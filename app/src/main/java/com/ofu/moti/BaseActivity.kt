f05a381fa9bb5562b2d7b6bd43120c493f8b8541
yuchocopie <pointu341@gmail.com>
2021-01-23 06:11:13 +0900

    🧀  add base file

---
+++ app/src/main/java/com/ofu/moti/BaseActivity.kt
@@ -0,0 +1,18 @@
+package com.ofu.moti
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+
+abstract class BaseActivity<B : ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+}
---
+++ app/src/main/java/com/ofu/moti/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package com.ofu.moti
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel() {}
