f0b46ea4efb93324e4b2eba87ca1e7bf89b93df8
yuchocopie <pointu341@gmail.com>
2021-01-23 12:00:16 +0900

    🧸 add userAgreement web connection

--- app/src/main/java/com/ofu/moti/login/LoginViewModel.kt
+++ app/src/main/java/com/ofu/moti/login/LoginViewModel.kt
@@ -1,15 +1,28 @@
 package com.ofu.moti.login
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import com.ofu.moti.BaseViewModel
 
-class LoginViewModel :BaseViewModel(){
+class LoginViewModel :BaseViewModel() {
 
-    enum class SignUpFragment {Birthday,Complete,Gender,NickName,EXIT}
+    enum class SignUpFragment { Birthday, Complete, Gender, NickName, EXIT }
 
-    val signUpFragment:MutableLiveData<SignUpFragment> = MutableLiveData()
+    val signUpFragment: MutableLiveData<SignUpFragment> = MutableLiveData()
 
-    fun onClickNextFragment(fragment:SignUpFragment){
+    fun onClickNextFragment(fragment: SignUpFragment) {
         signUpFragment.postValue(fragment)
     }
+
+    fun onClickUserAgreement(context: Context) {
+        context.startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.notion.so/ahobsu/MOTI-35d01dd331bb4aa0915c33d28d60b63c")
+            )
+        )
+    }
+
 }
