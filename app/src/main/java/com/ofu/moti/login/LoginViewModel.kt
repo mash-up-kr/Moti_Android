06edb33b97fa836abe6f192137850daa25b94a7e
yuchocopie <pointu341@gmail.com>
2021-01-25 02:22:40 +0900

    🧸 add sign up select gender

--- app/src/main/java/com/ofu/moti/login/LoginViewModel.kt
+++ app/src/main/java/com/ofu/moti/login/LoginViewModel.kt
@@ -3,25 +3,34 @@ package com.ofu.moti.login
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.ofu.moti.BaseViewModel
 
 class LoginViewModel :BaseViewModel() {
 
     enum class SignUpFragment { Birthday, Complete, Gender, NickName, EXIT }
+    enum class Gender { MAN, WOMAN }
 
     val signUpFragment: MutableLiveData<SignUpFragment> = MutableLiveData()
     val popFragment = MutableLiveData<Unit>()
     val etNickname = MutableLiveData<String>()
+    val userGender = MutableLiveData<Gender>()
     val toTheNextStep = MutableLiveData<Boolean>(false)
 
-    fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
+    fun onSelectGender(gender: Gender) {
+        toTheNextStep.postValue(true)
+        userGender.postValue(gender)
+    }
+
+    fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         toTheNextStep.postValue(!s.isNullOrBlank())
     }
-    fun onClickNextFragment(fragment: SignUpFragment) {
-        signUpFragment.postValue(fragment)
+
+    fun onClickNextFragment(fragment: SignUpFragment, next: Boolean) {
+        if (next) {
+            signUpFragment.postValue(fragment)
+        }
     }
 
     fun onClickPopFragment() {
