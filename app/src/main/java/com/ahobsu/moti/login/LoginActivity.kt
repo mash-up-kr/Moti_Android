ab8de21d8849f32e19307736cbebd92ca8732c66
yuchocopie <pointu341@gmail.com>
2021-02-17 17:19:43 +0900

    add firebase login api

--- app/src/main/java/com/ahobsu/moti/login/LoginActivity.kt
+++ app/src/main/java/com/ahobsu/moti/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.ahobsu.moti.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -9,20 +10,76 @@ import com.ahobsu.moti.BaseActivity
 import com.ahobsu.moti.R
 import com.ahobsu.moti.databinding.ActivityLoginBinding
 import com.ahobsu.moti.main.MainActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
 
     private val loginViewModel by viewModels<LoginViewModel>()
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initMainDataBinding()
+
+        binding.btnLogin.setOnClickListener {
+            signIn()
+        }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
 
         loginViewModel.signUpFragment.observe(this, Observer {
             if (it == LoginViewModel.SignUpFragment.NickName) {
                 changeFragment(SignUpNicknameFragment.newInstance())
             }
         })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        Log.d(TAG, "requestCode:" + requestCode)
+        Log.d(TAG, "resultCode:" + resultCode)
+        Log.d(TAG, "data:" + data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                Log.d(TAG, "firebaseAuthWith  idToken :" + account.idToken)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
     }
 
     fun startMainActivity() {
@@ -43,5 +100,28 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
 
     private fun initMainDataBinding() {
         binding.viewModel = loginViewModel
+    }
+
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    val view = binding.login
+                    Snackbar.make(view, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
     }
 }
