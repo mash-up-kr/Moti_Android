e03fb4c00e90900aac2c66c757c680b358049caf
yuchocopie <pointu341@gmail.com>
2021-05-12 23:04:02 +0900

    🧸 answer domain 코드 추가

--- app/src/main/java/com/ahobsu/moti/domain/entity/Answer.kt
+++ app/src/main/java/com/ahobsu/moti/domain/entity/Answer.kt
@@ -1,7 +1,7 @@
 package com.ahobsu.moti.domain.entity
 
-data class SignIn(
-    val accessToken: String = "",
-    val refreshToken: String = "",
-    val signUp: Boolean = false
+data class Answer(
+    val content: String?,
+    val missionId: Int,
+    val file: String?
 )
--- app/src/main/java/com/ahobsu/moti/domain/repository/AnswerRepository.kt
+++ app/src/main/java/com/ahobsu/moti/domain/repository/AnswerRepository.kt
@@ -1,10 +1,8 @@
 package com.ahobsu.moti.domain.repository
 
-import com.ahobsu.moti.domain.entity.Mission
+import com.ahobsu.moti.domain.entity.Answer
 import io.reactivex.rxjava3.core.Single
 
-interface MissionRepository {
-    fun getMissionId(id:Int): Single<Mission>
-    fun getMissions(): Single<List<Mission>>
-    fun getRefreshMissions(): Single<List<Mission>>
+interface AnswerRepository {
+    fun postAnswer(answer: Answer): Single<Boolean>
 }
