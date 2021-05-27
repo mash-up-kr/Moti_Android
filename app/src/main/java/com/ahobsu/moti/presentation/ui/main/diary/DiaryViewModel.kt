7c262fcb308b18130c8bab60649cd6b973dceff0
yuchocopie <pointu341@gmail.com>
2021-05-27 09:26:46 +0900

    🧸 Add diary item model

--- app/src/main/java/com/ahobsu/moti/presentation/ui/main/diary/DiaryViewModel.kt
+++ app/src/main/java/com/ahobsu/moti/presentation/ui/main/diary/DiaryViewModel.kt
@@ -1,5 +1,6 @@
 package com.ahobsu.moti.presentation.ui.main.diary
 
+import androidx.lifecycle.MutableLiveData
 import com.ahobsu.moti.domain.repository.AnswerRepository
 import com.ahobsu.moti.presentation.BaseViewModel
 
@@ -7,5 +8,17 @@ import com.ahobsu.moti.presentation.BaseViewModel
 class DiaryViewModel(
     private val answerRepository: AnswerRepository
 ) : BaseViewModel() {
-
+    private val _diaryList = MutableLiveData<List<DiaryItemModel>>()
+    val diaryList = _diaryList
 }
+
+data class DiaryItemModel(
+    val days: Int,
+    val month: String,
+    val title: String = "",
+    val content: String = "",
+    val imageUrl: String = "",
+    val isContent: Boolean = true,
+    val isImage: Boolean = false
+
+)
