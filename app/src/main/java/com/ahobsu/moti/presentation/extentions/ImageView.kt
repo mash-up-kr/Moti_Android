704be2f8be4fea9d53784c63e37370365597be41
yuchocopie <pointu341@gmail.com>
2021-05-27 11:25:42 +0900

    🧸 Add mypage circle image

--- app/src/main/java/com/ahobsu/moti/presentation/extentions/ImageView.kt
+++ app/src/main/java/com/ahobsu/moti/presentation/extentions/ImageView.kt
@@ -3,10 +3,20 @@ package com.ahobsu.moti.presentation.extentions
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("setImageUrl")
 fun ImageView.setImageUrl(url: String?) {
     Glide.with(context)
         .load(url)
+        .into(this)
+}
+
+@BindingAdapter("setCircleImageUrl")
+fun ImageView.setCircleImageUrl(url: String?) {
+    Glide.with(context)
+        .load(url)
+        .circleCrop()
         .into(this)
 }
--- app/src/main/java/com/ahobsu/moti/presentation/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/ahobsu/moti/presentation/ui/mypage/MyPageViewModel.kt
@@ -1,5 +1,6 @@
 package com.ahobsu.moti.presentation.ui.mypage
 
+import androidx.lifecycle.LiveData
 import com.ahobsu.moti.domain.repository.AnswerRepository
 import com.ahobsu.moti.presentation.BaseViewModel
 
@@ -8,4 +9,5 @@ class MyPageViewModel(
     private val answerRepository: AnswerRepository
 ) : BaseViewModel() {
 
+    val profileUrl = "https://avatars.githubusercontent.com/u/18034145?v=4"
 }
