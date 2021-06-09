835da79fec7ae1a30d6b6a9b9ec43d2a971cfddc
yuchocopie <pointu341@gmail.com>
2021-06-10 00:03:56 +0900

    🛠 rename date picker to spinner

---
+++ app/src/main/java/com/ahobsu/moti/presentation/cuscomview/DateSpinnerCustomView.kt
@@ -0,0 +1,45 @@
+package com.ahobsu.moti.presentation.cuscomview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.ahobsu.moti.databinding.CustomDateSpinnerBinding
+
+class DateSpinnerCustomView @JvmOverloads
+constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(
+    context,
+    attrs, defStyleAttr
+) {
+
+    private lateinit var viewBinding: CustomDateSpinnerBinding
+
+    init {
+        initLayout()
+        initDatePicker()
+    }
+
+    private fun initLayout() {
+        viewBinding = CustomDateSpinnerBinding.inflate(LayoutInflater.from(context), this)
+    }
+
+    private fun initDatePicker() {
+        viewBinding.pickerYear.maxValue = 2021
+        viewBinding.pickerYear.minValue = 1920
+        viewBinding.pickerMonth.maxValue = 12
+
+        viewBinding.pickerMonth.minValue = 1
+        viewBinding.pickerDay.maxValue = 31
+        viewBinding.pickerDay.minValue = 1
+
+        viewBinding.pickerYear.wrapSelectorWheel = false
+        viewBinding.pickerMonth.wrapSelectorWheel = false
+        viewBinding.pickerDay.wrapSelectorWheel = false
+
+        viewBinding.pickerYear.value = 2000
+    }
+
+    fun getDatePicker(): String {
+        return "${viewBinding.pickerYear.value}-${viewBinding.pickerMonth.value}-${viewBinding.pickerDay.value}"
+    }
+}
---
+++ app/src/main/java/com/ahobsu/moti/presentation/cuscomview/MonthSpinnerCustomView.kt
@@ -0,0 +1,47 @@
+package com.ahobsu.moti.presentation.cuscomview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.ahobsu.moti.databinding.CustomMonthSpinnerBinding
+
+class MonthSpinnerCustomView @JvmOverloads
+constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : LinearLayout(
+    context, attrs, defStyleAttr
+) {
+
+    private lateinit var viewBinding: CustomMonthSpinnerBinding
+
+    init {
+        initLayout()
+        initDatePicker()
+    }
+
+    private fun initLayout() {
+        viewBinding = CustomMonthSpinnerBinding.inflate(LayoutInflater.from(context), this)
+    }
+
+    private fun initDatePicker() {
+        viewBinding.pickerYear.maxValue = 2021
+        viewBinding.pickerYear.minValue = 1920
+
+        viewBinding.pickerMonth.maxValue = 12
+        viewBinding.pickerMonth.minValue = 1
+
+        viewBinding.pickerYear.wrapSelectorWheel = false
+        viewBinding.pickerMonth.wrapSelectorWheel = false
+
+        viewBinding.pickerYear.value = 2000
+    }
+
+    private fun setMonthPicker(year: Int, math: Int) {
+        viewBinding.pickerYear.value = year
+        viewBinding.pickerMonth.value = math
+
+    }
+
+    fun getMonthPicker(): String {
+        return "${viewBinding.pickerYear.value}-${viewBinding.pickerMonth.value}"
+    }
+}
