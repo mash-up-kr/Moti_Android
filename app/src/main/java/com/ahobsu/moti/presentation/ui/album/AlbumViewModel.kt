c2283822f31d83ed90b2462468ce34b9e4c76b54
yuchocopie <pointu341@gmail.com>
2021-06-09 21:10:31 +0900

    🧸 Add Album xml item

--- app/src/main/java/com/ahobsu/moti/presentation/ui/album/AlbumViewModel.kt
+++ app/src/main/java/com/ahobsu/moti/presentation/ui/album/AlbumViewModel.kt
@@ -1,5 +1,6 @@
 package com.ahobsu.moti.presentation.ui.album
 
+import com.ahobsu.moti.domain.entity.MissionCard
 import com.ahobsu.moti.domain.repository.AnswerRepository
 import com.ahobsu.moti.presentation.BaseViewModel
 
@@ -8,4 +9,15 @@ class AlbumViewModel(
     private val answerRepository: AnswerRepository
 ) : BaseViewModel() {
 
+}
+
+data class AlbumItemModel(
+    val answers: List<MissionCard>?
+) {
+    fun getCardPart(int: Int): String? {
+        val missionCards = answers?.filter {
+            it.cardPart == int
+        }
+        return if (!missionCards.isNullOrEmpty()) missionCards[0].cardPngUrl else null
+    }
 }
