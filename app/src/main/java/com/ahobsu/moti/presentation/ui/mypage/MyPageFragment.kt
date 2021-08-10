2a0d6e565f0633bbe4d9498d46db00b35bd55848
yuchocopie <pointu341@gmail.com>
2021-08-10 09:41:10 +0900

    🧸 add versoin textView

--- app/src/main/java/com/ahobsu/moti/presentation/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/ahobsu/moti/presentation/ui/mypage/MyPageFragment.kt
@@ -1,9 +1,11 @@
 package com.ahobsu.moti.presentation.ui.mypage
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
+import com.ahobsu.moti.BuildConfig
 import com.ahobsu.moti.R
 import com.ahobsu.moti.data.injection.Injection
 import com.ahobsu.moti.databinding.FragmentMypageBinding
@@ -22,12 +24,15 @@ class MyPageFragment :
         ).get(MyPageViewModel::class.java)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         binding.viewModel = viewModel
-        //TODO Version 설정
-        val version = "1.0.0"
 
+        val versionCode: Int = BuildConfig.VERSION_CODE
+        val versionName: String = BuildConfig.VERSION_NAME
+
+        binding.tvMypageVersion.text = "현재 $versionName"
         binding.ivMypageImage.setOnClickListener { view ->
             TedBottomPicker.with(mContext as FragmentActivity?)
                 .show {
@@ -41,7 +46,8 @@ class MyPageFragment :
             val address = arrayOf("heesoo0203@gmail.com")
             email.putExtra(Intent.EXTRA_EMAIL, address)
             email.putExtra(Intent.EXTRA_SUBJECT, "MOTI 문의하기")
-            email.putExtra(Intent.EXTRA_TEXT, "version: $version")
+            email.putExtra(Intent.EXTRA_TEXT,
+                " versionCode: $versionCode - versionName:$versionName")
             startActivity(email)
         }
         binding.btnMypageEdit.setOnClickListener {
